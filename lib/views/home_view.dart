296294c58abccb7ce2c32e3df38e6bd450d1cca9
João Cabral <joaocabral1232@gmail.com>
2022-05-27 17:16:55 -0400

    Add animation in listview

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
@@ -71,11 +72,14 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('What\'s the news?'),
       ),
       body: CustomScrollView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
         slivers: <Widget>[
           SliverList(
             delegate: SliverChildListDelegate(
@@ -130,50 +134,69 @@ class _HomeViewState extends State<HomeView> {
 
                     return Column(
                       children: [
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: controller.termsCountry.length,
-                          itemBuilder: (context, index) {
-                            final colorAvatar = Colors.primaries[
-                                random.nextInt(Colors.primaries.length)];
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 8.0, right: 8.0),
-                              child: Card(
-                                child: ListTile(
-                                  leading: ClipOval(
-                                    child: Container(
-                                      height: 40,
-                                      width: 40,
-                                      color: colorAvatar,
-                                      child: Center(
-                                        child: Text(
-                                          (index + 1).toString(),
-                                          style: GoogleFonts.roboto(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600,
-                                            color: Colors.white,
+                        AnimationLimiter(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            // physics: const BouncingScrollPhysics(
+                            //     parent: AlwaysScrollableScrollPhysics()),
+                            itemCount: controller.termsCountry.length,
+                            itemBuilder: (context, index) {
+                              final colorAvatar = Colors.primaries[
+                                  random.nextInt(Colors.primaries.length)];
+                              return AnimationConfiguration.staggeredList(
+                                position: index,
+                                delay: const Duration(milliseconds: 100),
+                                child: SlideAnimation(
+                                  duration: const Duration(milliseconds: 2500),
+                                  curve: Curves.fastLinearToSlowEaseIn,
+                                  child: FadeInAnimation(
+                                    curve: Curves.fastLinearToSlowEaseIn,
+                                    duration:
+                                        const Duration(milliseconds: 2500),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 8.0, right: 8.0),
+                                      child: Card(
+                                        child: ListTile(
+                                          leading: ClipOval(
+                                            child: Container(
+                                              height: 40,
+                                              width: 40,
+                                              color: colorAvatar,
+                                              child: Center(
+                                                child: Text(
+                                                  (index + 1).toString(),
+                                                  style: GoogleFonts.roboto(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.w600,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
                                           ),
+                                          title: Text(
+                                              controller.termsCountry[index]),
+                                          onTap: () {
+                                            Navigator.pushNamed(
+                                              context,
+                                              Routes.details,
+                                              arguments: ArgumentsRoutesModel(
+                                                term: controller
+                                                    .termsCountry[index],
+                                                color: colorAvatar,
+                                              ),
+                                            );
+                                          },
                                         ),
                                       ),
                                     ),
                                   ),
-                                  title: Text(controller.termsCountry[index]),
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                      context,
-                                      Routes.details,
-                                      arguments: ArgumentsRoutesModel(
-                                        term: controller.termsCountry[index],
-                                        color: colorAvatar,
-                                      ),
-                                    );
-                                  },
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
                         checkForAd(),
                       ],
