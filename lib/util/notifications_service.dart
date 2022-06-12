a3688183129552b626b92850860a09bc4e30c364
João Cabral <joaocabral1232@gmail.com>
2022-06-12 01:23:14 -0400

    feat: create notification

---
+++ lib/util/notifications_service.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:wtn_project/routes/routes.dart';
+
+//TODO: colocar essa classe na pasta de model
+class CustomNotification {
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+
+  CustomNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+}
+
+class NotificationService {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  late AndroidNotificationDetails androidDetails;
+
+  NotificationService() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    setupNotifications();
+  }
+
+  setupNotifications() async {
+    await setupTimezone();
+    await initializeNotifications();
+  }
+
+  Future<void> setupTimezone() async {
+    tz.initializeTimeZones();
+    final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+  }
+
+  initializeNotifications() async {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const iOS = IOSInitializationSettings();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(android: android, iOS: iOS),
+      onSelectNotification: onSelectNotification,
+    );
+  }
+
+  onSelectNotification(String? payload) {
+    if (payload != null && payload.isNotEmpty) {
+      Navigator.of(Routes.navigatorKey!.currentContext!)
+          .pushReplacementNamed(payload);
+    }
+  }
+
+  showNotificationScheduled(CustomNotification notification) async {
+    final date = DateTime.now().add(const Duration(hours: 9));
+
+    const androidDetails = AndroidNotificationDetails(
+      'terms_notification',
+      'hot_terms_notification',
+      channelDescription: 'This canal is show hot google search',
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+
+    flutterLocalNotificationsPlugin.zonedSchedule(
+      notification.id,
+      notification.title,
+      notification.body,
+      tz.TZDateTime.from(date, tz.local),
+      const NotificationDetails(
+        android: androidDetails,
+      ),
+      payload: notification.payload,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  checkForNotifications() async {
+    final details =
+        await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+    if (details != null && details.didNotificationLaunchApp) {
+      onSelectNotification(details.payload);
+    }
+  }
+}
