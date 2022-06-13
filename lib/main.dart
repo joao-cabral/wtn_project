c17a5a564359e8c8a749db48698fec4066d90500
João Cabral <joaocabral1232@gmail.com>
2022-06-12 20:47:25 -0400

    chore: remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -28,36 +28,3 @@ void main() {
     ),
   );
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-  
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         Provider<NotificationService>(
-//           create: (context) => NotificationService(),
-//         ),
-//         Provider<TermsRepository>(
-//           create: (context) => TermsRepository(),
-//         ),
-//         ChangeNotifierProvider<TermsController>(
-//           create: (context) => TermsController(
-//             termsRepository: context.read<TermsRepository>(),
-//           ),
-//         ),
-//       ],
-//       child: MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         title: 'What\'s the news?',
-//         theme: ThemeData(
-//           primarySwatch: Colors.blue,
-//         ),
-//         routes: Routes.list,
-//         initialRoute: Routes.splashscreen,
-//         navigatorKey: Routes.navigatorKey,
-//       ),
-//     );
-//   }
-// }
