88fdbbea4bd8f3a259d88712006a744a0161decd
João Cabral <joaocabral1232@gmail.com>
2022-06-12 20:50:13 -0400

    fix: change import in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wtn_project/main.dart';
+import 'package:wtn_project/views/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
